d06360c681232365c4ed870f9359b1e1aeea13bf
JGeli <88524940+jGeli@users.noreply.github.com>
2022-05-01 21:45:36 +0800

    Wew

--- GsmCron/test4.js
+++ GsmCron/test4.js
@@ -8,8 +8,6 @@ const serialportgsm = require('serialport-gsm');
 let port;
 let no = 4;
 
-
-
 serialportgsm.list((err,result) => {
         port = result[no] && result[no].path ;
 });
@@ -18,77 +16,42 @@ serialportgsm.list((err,result) => {
 function crun(){
 
     const ls = spawn("node", ["gsmCron4.js"]);
-
     
     ls.stdout.on("data", (data) => {
-        // console.log(data)
-        console.log(data == 'stop')
-    console.log(`stdout: ${data}`);
-    });
+        console.log(`stdout: ${data}`);
+        });
+        
+        ls.stderr.on("data", (data) => {
+        console.error(`stderr: ${data}`);
+        // crun();
+        });
+        
+        ls.on("close", (code) => {
     
-    ls.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-    });
+          console.log(`child process exited with code ${code}`);
+          if(code == 230){
+            console.log("Tunaay  na errror")
+            return process.exit()
+          }
     
-    ls.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-    crun();
-    // setTimeout(() => {
-    //     console.log('Repeating!!')
-    //     crun()
-    // }, 1000)
-    });
-}
-
-
-function init(){
-
-
-dbs.getConnection(function(err, connection) {
-
-      if(err) return console.log('DB Error!');
-      console.log('Db Connected')
-
-      db.sequelize.sync(
-        ).then(() => { 
-        // initial();
-        return crun();
+            crun();
+        });
+    }
     
-    })
-    .catch(err => {
-        console.log(err)
-    });
-    // console.log(connection)
-  });
-
-
-  setInterval( async () => {
-    if(port){
-
-        let messages = await MessageModel.getIncompleteMessage();
-        messages.forEach((a) => {
-            let { id, Mobtels } = a;
-            let ind = Mobtels.find(ab => !ab.isSent);
-            if(!ind){
-                MessageModel.setMessageComplete(id);
-            }
-        })
-        } else {
-            console.log('No Port Available')
-        }
-}, 30000)
-
-}
-
-init();
-
-// MessageModel.resetMessages()
-
-
-
-
-// device1.checkDeviceStatus();
-
-// SmsProcess.start();
-// console.log(obj.length);
-// console.log(objs.length);
+    function init(){
+    return dbs.getConnection(function(err, connection) {
+    
+          if(err) return console.log('DB Error!');
+          console.log('Db Connected')
+          crun();
+      });
+    
+    
+    
+    
+    }
+    
+    init();
+    
+    // MessageModel.resetMessages()
+    
